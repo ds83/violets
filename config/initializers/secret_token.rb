79a384aecb68c86c8f276e4dd8d922fbe7b2c2f7
Diman Sarandi <d_s_83@mail.ru>
2011-10-02 23:38:50 +0400

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Violets::Application.config.secret_token = 'ee19a07b6348b208f81421d6abade4ad02d7dab1d8f724613626b5c693510b2c8739f7e828803245b7179c6be875d080b0c9b641767d8adfa1ab09e3e42c1140'
